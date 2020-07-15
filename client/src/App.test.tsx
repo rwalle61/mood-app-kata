01729df1b0f95786189bb977ac92ff64b4ca70be
Richard Waller <Richard.Waller@ibm.com>
2020-07-15 11:21:24 +0100

    feat: sketch mood button behaviours

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { render, screen, within } from '@testing-library/react';
 import App from './App';
 
 describe('App - as a user', () => {
@@ -11,14 +12,15 @@ describe('App - as a user', () => {
       expect(screen.getByText('Check In')).toBeInTheDocument();
     });
     test('I can select my mood on a scale of 1-7', () => {
-      expect(screen.getByText('My mood:')).toBeInTheDocument();
-      expect(screen.getByText('1')).toBeInTheDocument();
-      expect(screen.getByText('2')).toBeInTheDocument();
-      expect(screen.getByText('3')).toBeInTheDocument();
-      expect(screen.getByText('4')).toBeInTheDocument();
-      expect(screen.getByText('5')).toBeInTheDocument();
-      expect(screen.getByText('6')).toBeInTheDocument();
-      expect(screen.getByText('7')).toBeInTheDocument();
+      const moodSelector = screen.getByText('My mood:').parentElement;
+      expect(moodSelector).toBeInTheDocument();
+      expect(within(moodSelector!).getByText('1')).toBeInTheDocument();
+      expect(within(moodSelector!).getByText('2')).toBeInTheDocument();
+      expect(within(moodSelector!).getByText('3')).toBeInTheDocument();
+      expect(within(moodSelector!).getByText('4')).toBeInTheDocument();
+      expect(within(moodSelector!).getByText('5')).toBeInTheDocument();
+      expect(within(moodSelector!).getByText('6')).toBeInTheDocument();
+      expect(within(moodSelector!).getByText('7')).toBeInTheDocument();
     });
     test("I can select how I'm feeling", () => {
       expect(screen.getByText("I'm feeling:")).toBeInTheDocument();
@@ -37,6 +39,39 @@ describe('App - as a user', () => {
       expect(screen.getByText('Mood Insights')).toBeInTheDocument();
       expect(screen.getByText('Average mood')).toBeInTheDocument();
       expect(screen.getByText('0 check-ins')).toBeInTheDocument();
+    });
+  });
+  describe('when I check in my mood', () => {
+    test('I see the check-ins count increase', () => {
+      const checkInButton = screen.getByText('Submit');
+      userEvent.click(checkInButton);
+
+      expect(screen.getByText('1 check-ins')).toBeInTheDocument();
+    });
+    test('I see my average mood matches what I submitted', () => {
+      const moodSelector = screen.getByText('My mood:').parentElement;
+      const mood4Button = within(moodSelector!).getByText('4');
+      userEvent.click(mood4Button);
+      const checkInButton = screen.getByText('Submit');
+      userEvent.click(checkInButton);
+
+      expect(screen.getByText('Average mood: 4')).toBeInTheDocument();
+    });
+    test('I see the check-in I submitted', () => {
+      const moodSelector = screen.getByText('My mood:').parentElement;
+      const mood4Button = within(moodSelector!).getByText('4');
+      userEvent.click(mood4Button);
+      const checkInButton = screen.getByText('Submit');
+      userEvent.click(checkInButton);
+
+      const [head, ...rows] = screen.getAllByRole('row');
+      expect(within(head).getByText('Date')).toBeInTheDocument();
+      expect(within(head).getByText('Time')).toBeInTheDocument();
+      expect(within(head).getByText('Mood')).toBeInTheDocument();
+      expect(within(head).getByText('Feeling')).toBeInTheDocument();
+      expect(within(head).getByText('Comment')).toBeInTheDocument();
+
+      expect(within(rows[0]).getByText('4')).toBeInTheDocument();
     });
   });
 });
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
 
-const App = (): JSX.Element => (
-  <div>
-    <h1>Check In</h1>
-    <h2>My mood:</h2>
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
-    <div>4</div>
-    <div>5</div>
-    <div>6</div>
-    <div>7</div>
-    <h2>I'm feeling:</h2>
-    <div>depressed</div>
-    <div>optimistic</div>
-    <div>bored</div>
-    <div>happy</div>
-    <h2>Comment:</h2>
-    <Button>Submit</Button>
-    <h1>Mood Insights</h1>
-    <h2>Average mood</h2>
-    <h2>0 check-ins</h2>
-  </div>
-);
+const App = (): JSX.Element => {
+  const [checkIns, setCheckIns] = useState(0);
+  const [averageMood, setAverageMood] = useState(0);
+
+  const onCheckIn = () => {
+    setCheckIns(checkIns + 1);
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>Check In</h1>
+        <div>
+          <h2>My mood:</h2>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div
+            onClick={() => {
+              setAverageMood(4);
+            }}
+          >
+            4
+          </div>
+          <div>5</div>
+          <div>6</div>
+          <div>7</div>
+        </div>
+        <div>
+          <h2>I'm feeling:</h2>
+          <div>depressed</div>
+          <div>optimistic</div>
+          <div>bored</div>
+          <div>happy</div>
+        </div>
+        <div>
+          <h2>Comment:</h2>
+        </div>
+        <Button onClick={onCheckIn}>Submit</Button>
+      </div>
+      <div>
+        <h1>Mood Insights</h1>
+        <h2>{`Average mood${averageMood ? `: ${averageMood}` : ''}`}</h2>
+        <h2>{`${checkIns} check-ins`}</h2>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Mood</th>
+              <th>Feeling</th>
+              <th>Comment</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>1</td>
+              <td>4</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  );
+};
 
 export default App;
